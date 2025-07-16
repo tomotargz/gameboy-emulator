763f1027d71f96b96383b9a735aec05d3a47c573
Tomota Nakamura <tnakamur@redhat.com>
2025-07-16 22:46:58 +0900

    Implement the minimum cpu

--- src/cpu.rs
+++ src/cpu.rs
@@ -13,9 +13,24 @@ pub struct Cpu {
 }
 
 impl Cpu {
+    pub fn emulate_cycle(&mut self, bus: &Peripherals) {
+        self.fetch(bus);
+    }
+
     pub fn fetch(&mut self, bus: &Peripherals) {
         self.ctx.opcode = bus.read(self.regs.pc);
         self.regs.pc = self.regs.pc.wrapping_add(1);
         self.ctx.cb = false;
+    }
+
+    pub fn decode(&mut self, bus: &Peripherals) {
+        match self.ctx.opcode {
+            0x00 => self.nop(bus),
+            _ => panic!("Not implemented: {:02x}", self.ctx.opcode),
+        }
+    }
+
+    pub fn nop(&mut self, bus: &Peripherals) {
+        self.fetch(bus);
     }
 }
