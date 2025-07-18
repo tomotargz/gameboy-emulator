0053970bceb1b048113c4a88de59c64bcc5c9191
Tomota Nakamura <tnakamur@redhat.com>
2025-07-18 23:13:26 +0900

    Implement IO8<Reg8> and IO16<Reg16> traits

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,4 @@
+use crate::operand::{IO8, IO16, Reg8, Reg16};
 use crate::peripherals::Peripherals;
 use crate::registers::Registers;
 
@@ -32,5 +33,55 @@ impl Cpu {
 
     pub fn nop(&mut self, bus: &Peripherals) {
         self.fetch(bus);
+    }
+}
+
+impl IO8<Reg8> for Cpu {
+    fn read8(&mut self, _: &Peripherals, src: Reg8) -> Option<u8> {
+        Some(match src {
+            Reg8::A => self.regs.a,
+            Reg8::B => self.regs.b,
+            Reg8::C => self.regs.c,
+            Reg8::D => self.regs.d,
+            Reg8::E => self.regs.e,
+            Reg8::F => self.regs.f,
+            Reg8::H => self.regs.h,
+            Reg8::L => self.regs.l,
+        })
+    }
+
+    fn write8(&mut self, bus: &mut Peripherals, dst: Reg8, val: u8) -> Option<()> {
+        Some(match dst {
+            Reg8::A => self.regs.a = val,
+            Reg8::B => self.regs.b = val,
+            Reg8::C => self.regs.c = val,
+            Reg8::D => self.regs.d = val,
+            Reg8::E => self.regs.e = val,
+            Reg8::F => self.regs.f = val,
+            Reg8::H => self.regs.h = val,
+            Reg8::L => self.regs.l = val,
+        })
+    }
+}
+
+impl IO16<Reg16> for Cpu {
+    fn read16(&mut self, _: &Peripherals, src: Reg16) -> Option<u16> {
+        Some(match src {
+            Reg16::AF => self.regs.af(),
+            Reg16::BC => self.regs.bc(),
+            Reg16::DE => self.regs.de(),
+            Reg16::HL => self.regs.hl(),
+            Reg16::SP => self.regs.sp,
+        })
+    }
+
+    fn write16(&mut self, bus: &mut Peripherals, dst: Reg16, val: u16) -> Option<()> {
+        Some(match dst {
+            Reg16::AF => self.regs.write_af(val),
+            Reg16::BC => self.regs.write_bc(val),
+            Reg16::DE => self.regs.write_de(val),
+            Reg16::HL => self.regs.write_hl(val),
+            Reg16::SP => self.regs.sp = val,
+        })
     }
 }
