c61ba976682aa0bbd0b6db0e5854629134018479
tomotargz <nakamuratomota@gmail.com>
2025-07-31 21:29:30 +0900

    Implement cp

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -45,4 +45,18 @@ impl Cpu {
             },
         });
     }
+
+    pub fn cp<S: Copy>(&mut self, bus: &Peripherals, src: S)
+    where
+        Self: IO8<S>,
+    {
+        if let Some(v) = self.read8(bus, src) {
+            let (result, carry) = self.regs.a.overflowing_sub(v);
+            self.regs.set_zf(result == 0);
+            self.regs.set_nf(true);
+            self.regs.set_hf((self.regs.a & 0xf) < (v & 0xf));
+            self.regs.set_cf(carry);
+            self.fetch(bus);
+        }
+    }
 }
