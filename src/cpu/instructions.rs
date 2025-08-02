7e09944bc2a4d071f2b1eacb6203dbffc566c516
tomotargz <nakamuratomota@gmail.com>
2025-08-02 12:30:38 +0900

    Implemente push

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -1,7 +1,7 @@
 use crate::{
     cpu::Cpu,
     instructions::{go, step},
-    operand::{IO8, IO16},
+    operand::{IO8, IO16, Reg16},
     peripherals::Peripherals,
 };
 
@@ -153,6 +153,59 @@ impl Cpu {
                 go!(1);
             },
             1: if self.write8(bus, src, VAL8.load(Relaxed)).is_some() {
+                go!(0);
+                self.fetch(bus);
+            },
+        });
+    }
+
+    pub fn bit<S: Copy>(&mut self, bus: &Peripherals, bit: usize, src: S)
+    where
+        Self: IO8<S>,
+    {
+        if let Some(mut v) = self.read8(bus, src) {
+            v &= 1 << bit;
+            self.regs.set_zf(v == 0);
+            self.regs.set_nf(false);
+            self.regs.set_hf(true);
+            self.fetch(bus);
+        }
+    }
+
+    pub fn push16(&mut self, bus: &mut Peripherals, val: u16) -> Option<()> {
+        step!(None, {
+            0: {
+                go!(1);
+                return None;
+            },
+            1 : {
+                let [lo, hi] = u16::to_le_bytes(val);
+                self.regs.sp = self.regs.sp.wrapping_sub(1);
+                bus.write(self.regs.sp, hi);
+                VAL8.store(lo, Relaxed);
+                go!(2);
+                return None;
+            },
+            2: {
+                self.regs.sp = self.regs.sp.wrapping_sub(1);
+                bus.write(self.regs.sp, VAL8.load(Relaxed));
+                go!(3);
+                return None;
+            },
+            3: return Some(go!(0)),
+        });
+    }
+
+    pub fn push(&mut self, bus: &mut Peripherals, src: Reg16) {
+        step!((), {
+            0: {
+                VAL16.store(self.read16(bus, src).unwrap(), Relaxed);
+                go!(1);
+            },
+            1: if self.push16(bus, VAL16.load(Relaxed)).is_some() {
+                go!(2);
+            },
+            2: {
                 go!(0);
                 self.fetch(bus);
             },
